587cae659c30fde0d3fb815d7938e735e18c24b2
Rifazul-Islam <rifazul50@gmail.com>
2024-09-25 11:19:14 +0600

    pagination insert

--- src/pages/Home/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Home/Testimonials/Testimonials.jsx
@@ -12,7 +12,7 @@ const Testimonials = () => {
   const [allReview, setAllReview] = useState([]);
 
   useEffect(() => {
-    fetch("reviews.json")
+    fetch("http://localhost:5000/reviews")
       .then((res) => res.json())
       .then((data) => setAllReview(data));
   }, []);
--- src/pages/Order/OrderTab/OrderTab.jsx
+++ src/pages/Order/OrderTab/OrderTab.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import OrderCard from "../Order/OrderCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
 
 const OrderTab = ({ orders }) => {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
+
   return (
-    <div className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6">
-      {orders.map((items) => (
-        <OrderCard items={items} key={items._id} />
-      ))}
-    </div>
+    <Swiper pagination={pagination} modules={[Pagination]} className="mySwiper">
+      <SwiperSlide>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6">
+          {orders.map((items) => (
+            <OrderCard items={items} key={items._id} />
+          ))}
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
 };
 
