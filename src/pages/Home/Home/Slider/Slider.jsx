8d38ab60591a133f42a515fafbd74cef20725249
Rifazul-Islam <rifazul50@gmail.com>
2024-09-26 08:13:40 +0600

    other page design

--- src/pages/Home/Home/Slider/Slider.jsx
+++ src/pages/Home/Home/Slider/Slider.jsx
@@ -22,18 +22,18 @@ const Slider = () => {
       }}
       navigation={true}
       modules={[Autoplay, Pagination, Navigation]}
-      className="mySwiper md:h-[500px] "
+      className="mySwiper md:h-[500px] w-full"
     >
       <SwiperSlide>
         <div>
-          <img src="https://plus.unsplash.com/premium_photo-1726729328727-ed63661c8fc3?q=80&w=1396&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+          <img src="https://plus.unsplash.com/premium_photo-1680344513206-8f3420af1d22?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </div>
       </SwiperSlide>
       <SwiperSlide>
         <div>
           <img
-            className=""
-            src="https://plus.unsplash.com/premium_photo-1686878940830-9031355ec98c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="w-full"
+            src="https://media.istockphoto.com/id/176602698/photo/potato-and-wheat-field.jpg?s=612x612&w=0&k=20&c=Q1xwRlhD24P4AA_UQZz4I-Wmo-nPQajiHSOnwCDYm2E="
             alt="slider"
           />
         </div>
