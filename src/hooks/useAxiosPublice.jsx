bc07533a37b7ca774b20142039fa2fec4cd56b63
Rifazul-Islam <rifazul50@gmail.com>
2024-10-02 07:37:07 +0600

    axios publice hook create

---
+++ src/hooks/useAxiosPublice.jsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const axiosPublice = axios.create({
+  baseURL: "http://localhost:5000",
+});
+
+const useAxiosPublice = () => {
+  return axiosPublice;
+};
+
+export default useAxiosPublice;
