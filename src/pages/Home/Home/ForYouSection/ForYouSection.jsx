a33e9ab39373c6b9ce9d56c64c6b0258f43f2675
Rifazul-Islam <rifazul50@gmail.com>
2024-10-17 21:09:51 +0600

    latest Products section create

--- src/pages/Home/Home/ForYouSection/ForYouSection.jsx
+++ src/pages/Home/Home/ForYouSection/ForYouSection.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { json } from "react-router-dom";
 
 const ForYouSection = () => {
+  const [products, setProducts] = useState();
+  useEffect(() => {
+    fetch("/newProduct.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  // console.log(products);
   return (
-    <div>
-      <h3>this is O </h3>
+    <div className=" my-10">
+      <h3 className="text-xl mb-4 font-inter font-medium pl-2 ">
+        Letest Products
+      </h3>
+      <div className="grid grid-cols-1 lg:grid-cols-5 gap-4 px-3 cursor-pointer">
+        {products?.map((item) => (
+          <div key={item?.id} className="bg-slate-50 border-2 rounded-md">
+            <img className="h-full" src={item?.image} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home/MenuItems/MenuItems.jsx
+++ src/pages/Home/Home/MenuItems/MenuItems.jsx
@@ -14,67 +14,12 @@ const MenuItems = () => {
     },
   });
 
-  console.log(categories);
-  // console.log("check the Popurlar Data", popular);
-  // const categories = [
-  //   {
-  //     name: "Frozen Mixed Vegetables",
-  //     image: "https://i.ibb.co.com/X2yF0xR/flow.png",
-  //     category: "Frozen",
-  //     backgroundColor: "#E3F2FD",
-  //     changeImage: true,
-  //   },
-  //   {
-  //     name: "Premium Beef Steak",
-  //     image: "https://i.ibb.co.com/VTW2fsS/meat.png",
-  //     category: "Fish",
-  //     backgroundColor: "#FFEBEE",
-  //     changeImage: true,
-  //   },
-  //   {
-  //     name: "Organic Whole Milk",
-  //     image: "https://i.ibb.co.com/0Kyvd1X/organic-milk.png",
-  //     category: "Milk",
-  //     backgroundColor: "#F3E5F5",
-  //     changeImage: true,
-  //   },
-  //   {
-  //     id: 4,
-  //     name: "Fresh Orange Juice",
-  //     image: "https://i.ibb.co.com/gJPNRMN/joss.png",
-  //     category: "Beverages",
-  //     backgroundColor: "#FFF3E0",
-  //     changeImage: true,
-  //   },
-
-  //   {
-  //     name: "Ripe Red Strawberries",
-  //     image: "https://i.ibb.co.com/3rX6grW/lici.png",
-  //     category: "Fruits",
-  //     backgroundColor: "#FCE4EC",
-  //     changeImage: true,
-  //   },
-
-  //   {
-  //     name: "Fresh Atlantic Salmon",
-  //     image: "https://i.ibb.co.com/VghFNcN/vagita.png",
-  //     category: "Meat",
-  //     backgroundColor: "#FFF3E0",
-  //     changeImage: true,
-  //   },
-  //   {
-  //     name: "Fresh Broccoli",
-  //     image: "https://i.ibb.co.com/J2n4bjs/fico.png",
-  //     category: "Vegetables",
-  //     backgroundColor: "#E8F5E9",
-  //     changeImage: true,
-  //   },
-  // ];
+  // console.log(categories);
 
   return (
     <div className="mb-16 bg-base-100">
-      <h2 className="text-xl font-inter font-medium px-16 "> Categories </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-2 mb-8 px-16  ">
+      <h2 className="text-[22px] font-inter font-medium"> Categories </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 mt-2 mb-8   ">
         {categories.map((item) => (
           <MenuCard item={item} key={item.id} />
         ))}
